83480d7ce794d813705c8eb8ac23754033f5a6d6
David Pires <david.ferreira.pires@everis.com>
2018-06-21 17:08:35 +0100

    Added storage plugin

---
+++ Subble.Core/src/Storage/IDirectory.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+
+namespace Subble.Core.Storage
+{
+    /// <summary>
+    /// Provides an API for Plugins to access the file system
+    /// </summary>
+    public interface IDirectory
+    {
+        /// <summary>
+        /// Temporary folder, that is frequently deleted
+        /// </summary>
+        DirectoryInfo TempFolder { get; }
+
+        /// <summary>
+        /// Diretory to store data local to this instalation, 
+        /// this files should not sync between instances
+        /// </summary>
+        DirectoryInfo LocalDirectory { get; }
+
+        /// <summary>
+        /// Directory to store data that may be sync between instances
+        /// </summary>
+        DirectoryInfo SyncDiretory { get; }
+
+        /// <summary>
+        /// Returns the directory reserved to that plugin
+        /// </summary>
+        /// <param name="pluginGuid">guid of plugin</param>
+        /// <param name="sync">set if directory should sync between instances</param>
+        /// <returns></returns>
+        DirectoryInfo GetDirectory(string pluginGuid, bool sync = false);
+
+        /// <summary>
+        /// Set the default temporary directory
+        /// </summary>
+        /// <param name="directory"></param>
+        void SetTempFolder(DirectoryInfo directory);
+
+        /// <summary>
+        /// Set the local directory path
+        /// </summary>
+        /// <param name="directory"></param>
+        void SetLocalDirectory(DirectoryInfo directory);
+
+        /// <summary>
+        /// Set the shared directory path
+        /// </summary>
+        /// <param name="directory"></param>
+        void SetSyncDirectory(DirectoryInfo directory);
+    }
+}
